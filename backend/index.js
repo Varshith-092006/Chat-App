4a6c30aade1d4121122b42c58c40a95ed8ec56dd
Varshtih <kollivarshith123@.com>
2025-06-21 16:38:50 +0530

    After change

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirnameResolved, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirnameResolved, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirnameResolved, "frontend","dist","index.html"));
   });
 }
 
